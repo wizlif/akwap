d3f8b7fe5a07bd879dbf5a3b6d716eabead2cf71
wizlif <wizlif.144@gmail.com>
2022-03-06 16:00:48 +0300

    fix: Entry points

--- src/index.ts
+++ src/index.ts
@@ -134,7 +134,9 @@ import TORORO from './districts/tororo';
 import WAKISO from './districts/wakiso';
 import YUMBE from './districts/yumbe';
 import ZOMBO from './districts/zombo';
-import { District } from './types';
+import { District, BaseCounty, Base, BaseSubCounty, BaseParish, BaseVillage } from './types';
+import { toCamelCase } from './utils/utils';
+import stringSimilarity = require('string-similarity');
 
 export const districts = Districts;
 
@@ -274,7 +276,7 @@ export const wakiso = WAKISO;
 export const yumbe = YUMBE;
 export const zombo = ZOMBO;
 
-export const _districtsHashMap: { [name: string]: District } = {
+const _districtsHashMap: { [name: string]: District } = {
   abim,
   adjumani,
   agago,
@@ -411,3 +413,177 @@ export const _districtsHashMap: { [name: string]: District } = {
   yumbe,
   zombo,
 };
+
+/***
+ * Helper methods
+ */
+export function getDistrict(district: string): District {
+  district = district.toLowerCase();
+  if (district.includes('-')) {
+    district = toCamelCase(district);
+  }
+
+  const ds = _districtsHashMap[district];
+
+  if (ds) {
+    return ds;
+  } else {
+    throw new Error('No such district');
+  }
+}
+
+export function getCounty(county: string, accuracy: number = 0.5): BaseCounty[] {
+  const counties = Object.entries(_districtsHashMap).map(([name, district]) => {
+    const _district: Base = {
+      id: district.id,
+      name: district.name,
+    };
+
+    return district.counties
+      .filter((ds) => {
+        const score = stringSimilarity.compareTwoStrings(ds.name.toLowerCase(), county.toLowerCase());
+        return score > accuracy;
+      })
+      .map((ds) => {
+        return {
+          id: ds.id,
+          name: ds.name,
+          district: _district,
+        } as BaseCounty;
+      });
+  });
+
+  return counties.reduce((accumulator, value) => accumulator.concat(value), []);
+}
+
+export function getSubCounty(subcounty: string, accuracy: number = 0.5): BaseSubCounty[] {
+  const counties = Object.entries(_districtsHashMap).map(([name, district]) => {
+    const _district: Base = {
+      id: district.id,
+      name: district.name,
+    };
+
+    return district.counties
+      .map((cs) => {
+        const _county: Base = {
+          id: cs.id,
+          name: cs.name,
+        };
+
+        return cs.sub_counties
+          .filter((es) => {
+            const score = stringSimilarity.compareTwoStrings(es.name.toLowerCase(), subcounty.toLowerCase());
+            return score > accuracy;
+          })
+          .map((es) => {
+            return {
+              id: es.id,
+              name: es.name,
+              district: _district,
+              county: _county,
+            } as BaseSubCounty;
+          });
+      })
+      .reduce((accumulator, value) => accumulator.concat(value), []);
+  });
+
+  return counties.reduce((accumulator, value) => accumulator.concat(value), []);
+}
+
+export function getParish(parish: string, accuracy: number = 0.5): BaseParish[] {
+  const counties = Object.entries(_districtsHashMap).map(([name, district]) => {
+    const _district: Base = {
+      id: district.id,
+      name: district.name,
+    };
+
+    return district.counties
+      .map((cs) => {
+        const _county: Base = {
+          id: cs.id,
+          name: cs.name,
+        };
+
+        return cs.sub_counties
+          .map((gs) => {
+            const _subcounty: Base = {
+              id: cs.id,
+              name: cs.name,
+            };
+
+            return gs.parishes
+              .filter((es) => {
+                const score = stringSimilarity.compareTwoStrings(es.name.toLowerCase(), parish.toLowerCase());
+                return score > accuracy;
+              })
+              .map((es) => {
+                return {
+                  id: es.id,
+                  name: es.name,
+                  district: _district,
+                  county: _county,
+                  sub_county: _subcounty,
+                } as BaseParish;
+              });
+          })
+          .reduce((accumulator, value) => accumulator.concat(value), []);
+      })
+      .reduce((accumulator, value) => accumulator.concat(value), []);
+  });
+
+  return counties.reduce((accumulator, value) => accumulator.concat(value), []);
+}
+
+export function getVillage(village: string, accuracy: number = 0.5): BaseVillage[] {
+  const counties = Object.entries(_districtsHashMap).map(([name, district]) => {
+    const _district: Base = {
+      id: district.id,
+      name: district.name,
+    };
+
+    return district.counties
+      .map((cs) => {
+        const _county: Base = {
+          id: cs.id,
+          name: cs.name,
+        };
+
+        return cs.sub_counties
+          .map((gs) => {
+            const _subcounty: Base = {
+              id: cs.id,
+              name: cs.name,
+            };
+
+            return gs.parishes
+              .map((ks) => {
+                const _parish: Base = {
+                  id: ks.id,
+                  name: ks.name,
+                };
+
+                return ks.villages
+                  .filter((es) => {
+                    const score = stringSimilarity.compareTwoStrings(es.name.toLowerCase(), village.toLowerCase());
+                    return score > accuracy;
+                  })
+                  .map((es) => {
+                    return {
+                      id: es.id,
+                      name: es.name,
+                      district: _district,
+                      county: _county,
+                      sub_county: _subcounty,
+                      parish: _parish,
+                    } as BaseVillage;
+                  });
+              })
+              .reduce((accumulator, value) => accumulator.concat(value), []);
+          })
+          .reduce((accumulator, value) => accumulator.concat(value), []);
+      })
+      .reduce((accumulator, value) => accumulator.concat(value), []);
+  });
+
+  return counties.reduce((accumulator, value) => accumulator.concat(value), []);
+}
